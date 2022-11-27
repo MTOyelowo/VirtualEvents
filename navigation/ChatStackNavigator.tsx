e88e7b35787a4f55a1e886709b28388ac0a40d08
M.T. Oyelowo <oyelowomayowa@gmail.com>
2022-11-27 16:58:07 +0100

    Chat Context

--- navigation/ChatStackNavigator.tsx
+++ navigation/ChatStackNavigator.tsx
@@ -1,14 +1,17 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ChatScreen from "../screens/Chat/ChatScreen";
 import ChatRoomScreen from "../screens/Chat/ChatRoomScreen";
+import ChatContextProvider from "../context/ChatContext";
 
 const Stack = createNativeStackNavigator();
 
 export default () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Chats" component={ChatScreen} />
-      <Stack.Screen name="ChatRoom" component={ChatRoomScreen} />
-    </Stack.Navigator>
+    <ChatContextProvider>
+      <Stack.Navigator>
+        <Stack.Screen name="Chats" component={ChatScreen} />
+        <Stack.Screen name="ChatRoom" component={ChatRoomScreen} />
+      </Stack.Navigator>
+    </ChatContextProvider>
   );
 };
--- screens/Chat/ChatScreen.tsx
+++ screens/Chat/ChatScreen.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { useChatContext } from "../../context/ChatContext";
 
 const ChatScreen = () => {
+  const { username } = useChatContext();
   return (
     <View>
-      <Text>ChatScreen</Text>
+      <Text>{username}</Text>
     </View>
   );
 };
