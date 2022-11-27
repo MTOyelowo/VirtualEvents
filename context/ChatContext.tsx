b2e30586c1754b4ac5b55b0288a1861cbd9d4691
M.T. Oyelowo <oyelowomayowa@gmail.com>
2022-11-27 17:23:12 +0100

    Connect User to Stream Chat SDK

--- context/ChatContext.tsx
+++ context/ChatContext.tsx
@@ -1,8 +1,49 @@
-import React, { createContext, useContext } from "react";
+import { useUserData } from "@nhost/react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { StreamChat } from "stream-chat";
 
 export const ChatContext = createContext({});
 
 const ChatContextProvider = ({ children }: { children: React.ReactNode }) => {
+  //component
+
+  const [chatClient, setChatClient] = useState<StreamChat>();
+
+  const user = useUserData();
+
+  useEffect(() => {
+    const initChat = async () => {
+      if (!user) {
+        return;
+      }
+
+      const client = StreamChat.getInstance("e9t46k3w5xrn");
+
+      // get info about authenticated user
+      // connect the user to stream chat
+
+      await client.connectUser(
+        {
+          id: user.id,
+          name: user.displayName,
+          image: user.avatarUrl,
+        },
+        client.devToken(user.id)
+      );
+      setChatClient(client);
+    };
+
+    initChat();
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      if (chatClient) {
+        chatClient.disconnectUser();
+      }
+    };
+  }, []);
+
   const value = { username: "Vadim" };
   return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
 };
